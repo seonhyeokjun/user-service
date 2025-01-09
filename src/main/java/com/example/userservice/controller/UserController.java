f21f8ecd8076f89631fda43752a769e6f5136b7c
seonhyeokjun <tjsrmatn@naver.com>
2025-01-09 20:19:19 +0900

    Micrometer 구현

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
@@ -30,6 +31,7 @@ public class UserController {
     }
 
     @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status() {
         return String.format("It's Working in User Service "
                 + ", port(local.server.port)=" + env.getProperty("local.server.port")
@@ -40,6 +42,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "users.status", longTask = true)
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
